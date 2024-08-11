cf0ea9f4b0390ffcf7e1fec6f4d30ea5f6c8514d
Akash Debnath <141671182+Akash007AD@users.noreply.github.com>
2024-08-11 12:53:24 +0530

    Update stock.routes.js

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -1,22 +1,4 @@
 
-/*
-import { Router } from 'express';
-import {
-    addStock,
-    getStockDetails,
-} from "../controllers/stock.controller.js"
-//import { verifyJWT } from "../middlewares/auth.middleware.js"
-
-const router = Router();
-
-//router.use(verifyJWT); 
-router.route("/:productId")
-    .get(getStockDetails)
-    .post(addStock);
-
-
-export default router;
-*/
 
 import { Router } from 'express';
 import {
@@ -29,6 +11,7 @@ const router = Router();
 // Routes to handle adding a new stock item and getting stock details by `product_id`
 router.route("/:productId")
     .get(getStockDetails)
+router.route("/")
     .post(addStock);
 
 export default router;
